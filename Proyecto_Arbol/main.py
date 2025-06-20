ddbff752c45b9e31edd506b8082dddce5b1c103e
Angela <ramosangelairina@gmail.com>
2025-06-20 08:43:18 -0300

    Cambios en main

--- Proyecto_Arbol/main.py
+++ Proyecto_Arbol/main.py
@@ -6,39 +6,43 @@ def main():
     """Función principal que solicita al usuario los valores del árbol y ejecuta las operaciones."""
     print("Bienvenido al programa de Árbol Binario de Búsqueda.")
 
-    entrada = input("Ingresá los valores del árbol separados por comas (ej: 8,3,10,1,6): ")
-    valores = validar_entrada(entrada)
-   
-    if not valores:
-        print("No se ingresaron valores válidos.")
-        exit()
+    while True:
+        entrada = input("Ingresá los valores del árbol separados por comas (ej: 8,3,10,1,6): ")
+        if not entrada.strip():
+            print("Entrada vacía. Por favor, ingresá al menos un valor.")
+            continue
 
-    raiz = None
-    for v in valores:
-        raiz = insertar(raiz, v)
+        valores = validar_entrada(entrada)
+        if not valores:
+            print("No se ingresaron valores válidos.")
+            exit()
 
-    print("\nElegí el recorrido que prefieras:")
-    print("1. Postorden")
-    print("2. Inorden")
-    print("3. Preorden")
-    opcion = input("Opción: ")
+        raiz = None
+        for v in valores:
+           raiz = insertar(raiz, v)
 
-    if opcion == "1":
-        postorden(raiz)
-    elif opcion == "2":
-        inorden(raiz)
-    elif opcion == "3":
-        preorden(raiz)
-    else:
-        print("Opción no válida.")
+        print("\nElegí el recorrido que prefieras:")
+        print("1. Postorden")
+        print("2. Inorden")
+        print("3. Preorden")
+        opcion = input("Opción: ")
 
-    print("\n\nÁrbol binario representado visualmente:")
-    dibujar_arbol(raiz)
+        if opcion == "1":
+         postorden(raiz)
+        elif opcion == "2":
+         inorden(raiz)
+        elif opcion == "3":
+         preorden(raiz)
+        else:
+            print("Opción no válida.")
 
-    continuar = input("\n¿Deseás crear otro árbol? (s/n): ").strip().lower()
-    if continuar != 's':
-        print("¡Gracias por usar el programa!")
-        return
+        print("\n\nÁrbol binario representado visualmente:")
+        dibujar_arbol(raiz)
+
+        continuar = input("\n¿Deseás crear otro árbol? (s/n): ").strip().lower()
+        if continuar != 's':
+            print("¡Gracias por usar el programa!")
+            break
 
 if __name__ == "__main__":
     main()
